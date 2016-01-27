3049693a9e126fdb250f46720b7ebd85508ded67
Juan Carlos Medina <jcmedina@newchannel.mx>
2016-01-27 14:04:35 -0600

    hoja de contacto

--- js/utils.js
+++ js/utils.js
@@ -6,3 +6,24 @@ $(document).ready( function(){
     $(this).removeClass('open');
   });
 })
+
+
+$(function() {
+  $("#contact #submit").click(function() {
+    var name = $("#form_name").val();
+    var email = $("#form_email").val();
+    var text = $("#msg_text").val();
+    var dataString = 'name='+ name + '&email=' + email + '&text=' + text;
+
+    $.ajax({
+      type: "POST",
+      url: "send.php",
+      data: dataString,
+      success: function(){
+      $('.success').fadeIn(1000);
+      }
+    });
+
+    return false;
+  });
+});
